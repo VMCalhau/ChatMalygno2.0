3f6bd080178403e0c53b1bb4d05841b5b80065a7
Vítor de Melo Calhau <42739346+VMCalhau@users.noreply.github.com>
2018-11-23 11:54:50 -0200

    Add files via upload

--- Principal.java
+++ Principal.java
@@ -1,13 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
-*/
-
-/**
- *
- * @author u18191
- */
 public class Principal extends javax.swing.JFrame {
 
     /**
